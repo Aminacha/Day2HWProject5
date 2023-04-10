73bf56bf44986d6d72e8457fe7f749be1b0490d8
gaiam <gaiam@DESKTOP-PATQCNV.fios-router.home>
2023-04-10 02:09:29 -0700

    Implemented getInfo5()

--- src/stashPushHW5/Project5Demo.java
+++ src/stashPushHW5/Project5Demo.java
@@ -2,11 +2,8 @@ package stashPushHW5;
 
 public class Project5Demo {
 
-	public static void main(String[] args) {
-		
-		
-		System.out.println("Stash Push HW 5");
+	public void getInfo5() {
 
+		System.out.println("Pushing code ...");
 	}
-
 }
